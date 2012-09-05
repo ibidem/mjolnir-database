dea15dba3e106a183d725aaea51724b5fb35f6c1
srcspider <source.spider@gmail.com>
2012-09-05 11:20:08 +0300

    fix

--- Trait/Model/Automaton.php
+++ Trait/Model/Automaton.php
@@ -38,7 +38,7 @@ trait Trait_Model_Automaton
 	 * @param array fields
 	 */
 	static function process(array $fields)
-	{
+	{		
 		static::inserter($fields, static::$automaton['fields'])->run();
 		static::$last_inserted_id = \app\SQL::last_inserted_id();
 	}
@@ -49,7 +49,16 @@ trait Trait_Model_Automaton
 	 */
 	static function update_process($id, array $fields) 
 	{
-		static::updater($id, $fields, static::$automaton['fields'])->run();
+		$update_fields = static::$automaton['fields'];
+		if (static::unique_key() !== 'id')
+		{
+			if (($key = \array_search(static::unique_key(), $update_fields)) !== false) 
+			{
+				unset($update_fields[$key]);
+			}
+		}
+		
+		static::updater($id, $fields, $update_fields)->run();
 		static::clear_entry_cache($id);
 	}
 	
