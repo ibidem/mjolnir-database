f46bf36c46c00f3110e671a3ab0d5e11300b8c8a
srcspider <source.spider@gmail.com>
2012-09-10 03:58:28 +0300

    refactored names to mjolnir

---
+++ +App/views/mjolnir/database/autofills/Model.php
@@ -0,0 +1,31 @@
+	# last_inserted_id, table, push, update, update_check
+	use \app\Trait_Model_Factory;
+	# stash, statement, snatch, inserter, updater
+	use \app\Trait_Model_Utilities;
+	# entries, entry, find, find_entry, clear_entry_cache, delete, count, exists
+	use \app\Trait_Model_Collection;
+	# check, process, update_process
+	use \app\Trait_Model_Automaton;
+	
+	/**
+	 * @var string
+	 */
+	protected static $table = 'table_name';
+	
+	/**
+	 * @var array
+	 */
+	protected static $field_format = [];
+	
+	/**
+	 * @var array
+	 */
+	protected static $automaton = array
+		(
+			'fields' => [ 'title' ],
+			'unique' => [ 'title' ],
+			'errors' => array
+				(
+					'title' => [ ':unique' => 'Entry with the same title already exists.' ],
+				)
+		);
