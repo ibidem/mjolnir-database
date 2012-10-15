cd0b7f1a4973176f7ee40acbf5ef5ca7c38381c7
srcspider <source.spider@gmail.com>
2012-10-15 16:09:08 +0300

    valid_number error

--- +App/config/mjolnir/general-errors.php
+++ +App/config/mjolnir/general-errors.php
@@ -1,6 +1,7 @@
 <?php return array
 	(
 		'not_empty' => 'Field is required.',
+		'valid_number' => 'Field must be valid number.',
 		':exists' => 'Entry does not exist.',
 		':unique' => 'Value must be unique.'
 	);
