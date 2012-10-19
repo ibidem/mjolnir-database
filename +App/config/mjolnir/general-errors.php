3e36e784a279feb18521b86fbad334105bfc2293
srcspider <source.spider@gmail.com>
2012-10-19 17:49:31 +0300

    :valid as default

--- +App/config/mjolnir/general-errors.php
+++ +App/config/mjolnir/general-errors.php
@@ -3,5 +3,6 @@
 		'not_empty' => 'Field is required.',
 		'valid_number' => 'Field must be valid number.',
 		':exists' => 'Entry does not exist.',
-		':unique' => 'Value must be unique.'
+		':unique' => 'Value must be unique.',
+		':valid' => 'Invalid value provided.'
 	);
