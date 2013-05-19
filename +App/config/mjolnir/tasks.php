81224eb261b578aecea318dcf6438d45c53c0304
srcspider <source.spider@gmail.com>
2013-05-20 01:17:16 +0300

    paradox tasks

--- +App/config/mjolnir/tasks.php
+++ +App/config/mjolnir/tasks.php
@@ -1,5 +1,69 @@
 <?php return array
 	(
+		'pdx:reset' => array
+			(
+				'category' => 'Database',
+				'description' => array
+					(
+						'Reset database. Latest version if no version is provided.',
+						'When resetting to a specific version you must provide the root '.
+						'channel to be used when determining which channels need to be '.
+						'at which version to for teh migration.'
+					),
+				'flags' => array
+					(
+						'version' => array
+							(
+								'type' => 'text',
+								'description' => 'Channel version; channel must also be provided.',
+								'short' => 'v',
+								'default' => false,
+							),
+						'app' => array
+							(
+								'type' => 'text',
+								'description' => 'Application channel or main channel.',
+								'short' => 'a',
+								'default' => false,
+							),
+					),
+			),
+		'pdx:uninstall' => array
+			(
+				'category' => 'Database',
+				'description' => array
+					(
+						'Uninstalls all database.',
+					),
+				'flags' => array
+					(
+						// empty
+					),
+			),
+		'pdx:upgrade' => array
+			(
+				'category' => 'Database',
+				'description' => array
+					(
+						'Upgrades all channels to latest version.',
+					),
+				'flags' => array
+					(
+						// empty
+					),
+			),
+		'pdx:status' => array
+			(
+				'category' => 'Database',
+				'description' => array
+					(
+						'Displays version information.',
+					),
+				'flags' => array
+					(
+						// empty
+					),
+			),
 		'db:sphinx' => array
 			(
 				'category' => 'Database',
